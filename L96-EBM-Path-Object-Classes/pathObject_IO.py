c85dd37e6444f1d03887ada25672c8eb8bfd84f7
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2022-10-25 17:03:56 +0100

    Addded some path object giles and started plotting effect of S

--- L96-EBM-Path-Object-Classes/pathObject_IO.py
+++ L96-EBM-Path-Object-Classes/pathObject_IO.py
@@ -1,7 +1,7 @@
 """
 Contains:
-- Locations where we save attractor data.
-- Functions for helping us to open attractor data.
+- Locations of attractor and relaxation data.
+- Functions for helping us to open attractor and relaxation data.
 """
 ########################################################################
 ## Imports
@@ -9,7 +9,6 @@ Contains:
 # Standard package imports
 import xarray as xr
 import numpy as np
-import random
 
 # Function to identify where we're running
 from pathlib import Path
@@ -24,28 +23,26 @@ def on_mac():
 
 # Data parent directory
 if on_mac():
-    # PD for Attractor Data: Should be SB, W and M.
-    attractor_data_pd = '/Users/cfn18/Documents/PhD-Work/Third-Year/Instanton-Work/L96-EBM-Instanton-Cleaned-Up/L96-EBM-Effect-of-S/Attractor-Data/'
-
+    # Location of Relaxation Data
+    relxation_data_pd = '/Users/cfn18/Documents/PhD-Work/Third-Year/Instanton-Work/L96-EBM-Instanton-Cleaned-Up/L96-EBM-Relaxations/Relaxation-Data/'
+    relaxation_from_mstate_pd = relxation_data_pd + '/Relaxing-from-M-State/'
+    relaxation_from_cube_pd = relxation_data_pd + '/Relaxing-from-Cube/'
+    # Location of Attractor Data: Should be SB, W and M.
+    attractor_data_pd = '/Users/cfn18/Documents/PhD-Work/Third-Year/Instanton-Work/L96-EBM-Instanton/Deterministic-Model/Effect-of-S/Attractor-Data/'
 else:
-    print('Please specify location of attractor data.')
+    # Location of Relaxation Data
+    print('Specify where relaxation data is stored.')
+    relxation_data_pd = None
 
 # Attractor File Names
-def attractor_folder_name(end_state, pd=attractor_data_pd, S=10):
-    "end_state, string: One of w, sb or m."
-    return pd + f'/S_{S:.3f}/{end_state.upper()}-State/'.replace('.', '_')
-
 def w_attractor_file_name(pd=attractor_data_pd, S=10):
-    sd = attractor_folder_name('w', pd=pd, S=S)
-    return sd + '1.nc'
+    return pd + f'/S_{S}/hot-attractor/L96-EBM-Trajectory1.nc'
 
 def sb_attractor_file_name(pd=attractor_data_pd, S=10):
-    sd = attractor_folder_name('sb', pd=pd, S=S)
-    return sd + '1.nc'
+    return pd + f'/S_{S}/cold-attractor/L96-EBM-Trajectory1.nc'
 
 def m_state_file_name(pd=attractor_data_pd, S=10):
-    sd = attractor_folder_name('m', pd=pd, S=S)
-    return sd + '1.nc'
+    return pd + f'/S_{S}/M-State/1.nc'
 
 ########################################################################
 ## Functions for opening determinsitic attractors & M-State
@@ -66,6 +63,11 @@ def get_m_state(pd=attractor_data_pd, S=10):
     file_name = m_state_file_name(pd=pd, S=S)
     return get_attractor(file_name)
 
+def get_smooth_m_state(pd=attractor_data_pd, S=10):
+    file_name = m_state_file_name(pd=pd, S=S)
+    m_state = get_attractor(file_name)
+    return m_state.interp({'time':np.arange(5.1, 105, 0.01)}, method='cubic')
+
 def ds_to_np(ds):
     "Converts ds point to np array."
     X = ds.X.values
@@ -76,18 +78,3 @@ def get_ds_points(ds, n):
     "Sample n points from a ds without replacament."
     time_points = np.random.choice(ds.time, n, replace=False)
     return [ds_to_np(ds.sel(time=x)) for x in time_points]
-
-def get_random_point(ds_file):
-    with xr.open_dataset(ds_file) as ds:
-        data_point = ds.sel(time=random.choice(ds.time))
-        xs = data_point.X.values
-        T = data_point.T.values
-    return np.append(xs, T)
-
-def get_random_w_point(S=10, pd=attractor_data_pd):
-    w_attractor_file = w_attractor_file_name(S=S, pd=pd)
-    return get_random_point(w_attractor_file)
-
-def get_random_sb_point(S=10, pd=attractor_data_pd):
-    sb_attractor_file = w_attractor_file_name(S=S, pd=pd)
-    return get_random_point(sb_attractor_file)
--- Plotting-Effect-of-S/plotting_IO.py
+++ Plotting-Effect-of-S/plotting_IO.py
@@ -9,7 +9,7 @@ Contains:
 # Standard package imports
 import xarray as xr
 import numpy as np
-import random
+import sys
 
 # Function to identify where we're running
 from pathlib import Path
@@ -19,18 +19,25 @@ def on_mac():
     return mac_home_dir == home_dir
 
 ########################################################################
-## Data Locations
+## Data + Custom import Locations
 ########################################################################
 
-# Data parent directory
+# Data parent adn attractor object directories
 if on_mac():
     # PD for Attractor Data: Should be SB, W and M.
     attractor_data_pd = '/Users/cfn18/Documents/PhD-Work/Third-Year/Instanton-Work/L96-EBM-Instanton-Cleaned-Up/L96-EBM-Effect-of-S/Attractor-Data/'
-
+    attractorObjectDirectory = '/Users/cfn18/Documents/PhD-Work/Third-Year/Instanton-Work/L96-EBM-Instanton-Cleaned-Up/L96-EBM-Effect-of-S/L96-EBM-Path-Object-Classes/'
 else:
     print('Please specify location of attractor data.')
 
-# Attractor File Names
+sys.path.append(attractorObjectDirectory)
+from attractors import L96EBMAttractor
+from plots import *
+
+########################################################################
+## Attractor File Names
+########################################################################
+
 def attractor_folder_name(end_state, pd=attractor_data_pd, S=10):
     "end_state, string: One of w, sb or m."
     return pd + f'/S_{S:.3f}/{end_state.upper()}-State/'.replace('.', '_')
@@ -52,7 +59,7 @@ def m_state_file_name(pd=attractor_data_pd, S=10):
 ########################################################################
 
 def get_attractor(file_name):
-    return xr.open_dataset(file_name)
+    return L96EBMAttractor(file_name)
 
 def get_w_attractor(pd=attractor_data_pd, S=10):
     file_name = w_attractor_file_name(pd=pd, S=S)
@@ -76,18 +83,3 @@ def get_ds_points(ds, n):
     "Sample n points from a ds without replacament."
     time_points = np.random.choice(ds.time, n, replace=False)
     return [ds_to_np(ds.sel(time=x)) for x in time_points]
-
-def get_random_point(ds_file):
-    with xr.open_dataset(ds_file) as ds:
-        data_point = ds.sel(time=random.choice(ds.time))
-        xs = data_point.X.values
-        T = data_point.T.values
-    return np.append(xs, T)
-
-def get_random_w_point(S=10, pd=attractor_data_pd):
-    w_attractor_file = w_attractor_file_name(S=S, pd=pd)
-    return get_random_point(w_attractor_file)
-
-def get_random_sb_point(S=10, pd=attractor_data_pd):
-    sb_attractor_file = w_attractor_file_name(S=S, pd=pd)
-    return get_random_point(sb_attractor_file)
